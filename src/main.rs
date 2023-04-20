af194dd80efeb0553d1429a1406967b849d836bf
Justin Frank <justinpfrank@protonmail.com>
2023-04-19 23:47:39 -0400

    recompute preds/succs rather than copy graph

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,17 @@
 use rand::prelude::*;
 use rayon::prelude::*;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::{Duration, Instant};
-
-//struct VecVecGraph {
-//    vs: Vec<Vec<usize>>,
-//}
-
-/*
-impl VecVecGraph {
-    fn new(num_vertices: usize) -> VecVecGraph {
-        VecVecGraph {
-            vs: (0..num_vertices)
-                .into_par_iter()
-                .map(|_| Vec::new())
-                .collect(),
-        }
-    }
-
-    // Does not check that edge doesn't already exist
-    fn add_edge(&mut self, v1: usize, v2: usize) {
-        self.vs[v1].push(v2);
-        self.vs[v2].push(v1);
-    }
-}
-*/
 
 struct CsrGraph {
     vertices: Vec<usize>,
     edges: Vec<usize>,
 }
 
-trait ParallelColorableGraph: Send + Sync {
+trait ParallelColorableGraph: Copy + Send + Sync {
     fn num_vertices(&self) -> usize;
     fn neighbors(&self, v: usize) -> &[usize];
     fn degree(&self, v: usize) -> usize {
@@ -66,30 +46,31 @@ impl ParallelColorableGraph for &CsrGraph {
     }
 }
 
-fn jones_plassmann<T>(graph: T, rho: &Vec<f64>) -> Vec<usize>
+fn jones_plassmann<T, W>(graph: T, rho: &Vec<W>) -> Vec<usize>
 where
     T: ParallelColorableGraph,
+    W: PartialOrd + Send + Sync,
 {
-    struct JPVertex {
-        pred: Vec<usize>,
-        succ: Vec<usize>,
-        counter: AtomicUsize,
-    }
+    fn jp_get_color<T, W>(graph: T, rho: &Vec<W>, colors: &Vec<AtomicUsize>, v: usize) -> usize
+    where
+        T: ParallelColorableGraph,
+        W: PartialOrd + Send + Sync,
+    {
+        let avail_colors: Vec<AtomicUsize> = (0..graph.degree(v) + 1)
+            .into_par_iter()
+            .map(AtomicUsize::new)
+            .collect();
 
-    fn jp_get_color(vs: &Vec<JPVertex>, colors: &Vec<AtomicUsize>, v: usize) -> usize {
-        let n = vs[v].pred.len();
-
-        // Atomics are written to with Relaxed ordering
-        // Works because all threads are joined before reading
-        let avail_colors: Vec<AtomicUsize> =
-            (0..n + 1).into_par_iter().map(AtomicUsize::new).collect();
-
-        vs[v].pred.par_iter().for_each(|u| {
-            let color = colors[*u].load(Ordering::Acquire);
-            if let Some(c) = avail_colors.get(color) {
-                c.store(usize::MAX, Ordering::Relaxed);
-            }
-        });
+        graph
+            .neighbors(v)
+            .par_iter()
+            .filter(|u| rho[**u] > rho[v])
+            .for_each(|u| {
+                let color = colors[*u].load(Ordering::Acquire);
+                if let Some(c) = avail_colors.get(color) {
+                    c.store(usize::MAX, Ordering::Relaxed);
+                }
+            });
 
         avail_colors
             .into_par_iter()
@@ -98,34 +79,27 @@ where
             .unwrap()
     }
 
-    fn jp_color(vs: &Vec<JPVertex>, colors: &Vec<AtomicUsize>, v: usize) {
-        colors[v].store(jp_get_color(vs, colors, v), Ordering::Release);
-        vs[v].succ.par_iter().for_each(|u| {
-            if vs[*u].counter.fetch_sub(1, Ordering::SeqCst) == 1 {
-                jp_color(vs, colors, *u);
-            }
-        });
-    }
-
-    let vs: Vec<_> = (0..graph.num_vertices())
-        .into_par_iter()
-        .map(|v| {
-            let mut preds = Vec::new();
-            let mut succs = Vec::new();
-            for u in graph.neighbors(v) {
-                if rho[*u] > rho[v] {
-                    preds.push(*u);
-                } else {
-                    succs.push(*u);
+    fn jp_color<T, W>(
+        graph: T,
+        rho: &Vec<W>,
+        colors: &Vec<AtomicUsize>,
+        counters: &Vec<AtomicUsize>,
+        v: usize,
+    ) where
+        T: ParallelColorableGraph,
+        W: PartialOrd + Send + Sync,
+    {
+        colors[v].store(jp_get_color(graph, rho, colors, v), Ordering::Release);
+        graph
+            .neighbors(v)
+            .par_iter()
+            .filter(|u| rho[**u] < rho[v])
+            .for_each(|u| {
+                if counters[*u].fetch_sub(1, Ordering::SeqCst) == 1 {
+                    jp_color(graph, rho, colors, counters, *u);
                 }
-            }
-            JPVertex {
-                counter: AtomicUsize::new(preds.len()),
-                pred: preds,
-                succ: succs,
-            }
-        })
-        .collect();
+            });
+    }
 
     // Atomics in this vector are accessed with store/load with Acquire/Release ordering
     // Relaxed probably works too, but this is safer
@@ -134,17 +108,33 @@ where
         .map(|i| AtomicUsize::new(i))
         .collect();
 
+    let counters: Vec<AtomicUsize> = (0..graph.num_vertices())
+        .into_par_iter()
+        .map(|v| {
+            AtomicUsize::new(
+                graph
+                    .neighbors(v)
+                    .par_iter()
+                    .filter(|u| rho[**u] > rho[v])
+                    .count(),
+            )
+        })
+        .collect();
+
     (0..graph.num_vertices())
         .into_par_iter()
-        .filter(|v| vs[*v].pred.len() == 0)
-        .for_each(|v| jp_color(&vs, &colors, v));
+        .filter(|v| {
+            graph
+                .neighbors(*v)
+                .par_iter()
+                .filter(|u| rho[**u] > rho[*v])
+                .count()
+                == 0
+        })
+        .for_each(|v| jp_color(graph, rho, &colors, &counters, v));
 
     // This should be compiled to a no-op
     colors.into_iter().map(AtomicUsize::into_inner).collect()
-
-    // ... but this is guaranteed :)
-    // actually not sure how it interacts with memory reordering guarantees
-    //unsafe { std::mem::transmute(colors) }
 }
 
 fn check_coloring<T>(graph: T, coloring: &Vec<usize>)
@@ -168,14 +158,16 @@ where
     }
 }
 
-fn make_random_graph(n: usize, p: f64) -> Vec<Vec<usize>> {
-    let mut graph: Vec<_> = rayon::iter::repeatn(vec![], n).collect();
+fn make_random_graph(n: usize, m: usize) -> Vec<Vec<usize>> {
     let mut rng = rand::thread_rng();
-    for i in 0..n {
-        for j in i + 1..n {
-            if rng.gen::<f64>() < p {
-                graph[i].push(j);
-                graph[j].push(i);
+    let mut graph: Vec<_> = rayon::iter::repeatn(vec![], n).collect();
+
+    for v in 0..n {
+        for _ in 0..rng.gen_range(0..m) {
+            let u = rng.gen_range(0..n);
+            if u != v && !graph[v].contains(&u) {
+                graph[v].push(u);
+                graph[u].push(v);
             }
         }
     }
@@ -183,19 +175,57 @@ fn make_random_graph(n: usize, p: f64) -> Vec<Vec<usize>> {
     graph
 }
 
-fn make_random_neighborhood_graph(n: usize, p: f64) -> Vec<Vec<usize>> {
-    let mut graph: Vec<_> = rayon::iter::repeatn(vec![], n).collect();
-    let mut rng = rand::thread_rng();
-    for i in 0..n {
-        for j in i + 1..n {
-            if rng.gen::<f64>() < p / ((j - i + 1) as f64).log2() {
-                graph[i].push(j);
-                graph[j].push(i);
+fn make_path_graph(n: usize) -> Vec<Vec<usize>> {
+    (0..n)
+        .into_par_iter()
+        .map(|i| {
+            if n == 0 {
+                vec![]
+            } else if i == 0 {
+                vec![1]
+            } else if i == n - 1 {
+                vec![i - 1]
+            } else {
+                vec![i - 1, i + 1]
             }
+        })
+        .collect()
+}
+
+fn make_path_csr_graph(n: usize) -> CsrGraph {
+    if n == 0 {
+        CsrGraph {
+            vertices: vec![0],
+            edges: vec![],
+        }
+    } else {
+        CsrGraph {
+            vertices: (0..n + 1)
+                .into_par_iter()
+                .map(|i| {
+                    if i == 0 {
+                        0
+                    } else if i == n {
+                        2 * i - 2
+                    } else {
+                        2 * i - 1
+                    }
+                })
+                .collect(),
+            edges: (0..2 * n - 2)
+                .into_par_iter()
+                .map(|i| {
+                    if i == 0 {
+                        1
+                    } else if i == 2 * n - 3 {
+                        n - 2
+                    } else {
+                        (i / 2) + ((i - 1) % 2)
+                    }
+                })
+                .collect(),
         }
     }
-
-    graph
 }
 
 fn make_random_order<T: ParallelColorableGraph>(graph: T) -> Vec<f64> {
@@ -226,7 +256,8 @@ fn make_llf_order<T: ParallelColorableGraph>(graph: T) -> Vec<f64> {
 }
 
 fn make_sl_order<T: ParallelColorableGraph>(graph: T) -> Vec<f64> {
-    let mut rho: Vec<f64> = rayon::iter::repeatn(0.0, graph.num_vertices()).collect();
+    let mut rho: Vec<_> = rayon::iter::repeatn(0.0, graph.num_vertices()).collect();
+    let mut to_remove: Vec<bool> = rayon::iter::repeatn(false, graph.num_vertices()).collect();
 
     let degrees: Vec<AtomicUsize> = (0..graph.num_vertices())
         .into_par_iter()
@@ -243,11 +274,10 @@ fn make_sl_order<T: ParallelColorableGraph>(graph: T) -> Vec<f64> {
             break;
         }
 
-        // TODO: use collect_into_vec to reuse rather than reallocate
-        let to_remove: Vec<bool> = degrees
+        degrees
             .par_iter()
             .map(|d| d.load(Ordering::Relaxed) <= threshold)
-            .collect();
+            .collect_into_vec(&mut to_remove);
 
         rho.par_iter_mut().enumerate().for_each_init(
             || rand::thread_rng(),
@@ -269,10 +299,15 @@ fn make_sl_order<T: ParallelColorableGraph>(graph: T) -> Vec<f64> {
     rho
 }
 
-fn test_coloring<T, F>(graph: T, gen_order: F, num_rounds: usize)
+fn make_sll_order<T: ParallelColorableGraph>(graph: T) -> Vec<f64> {
+    todo!()
+}
+
+fn test_coloring<T, W, F>(graph: T, gen_order: F, num_rounds: usize)
 where
     T: ParallelColorableGraph + Copy,
-    F: Fn(T) -> Vec<f64>,
+    W: PartialOrd + Send + Sync,
+    F: Fn(T) -> Vec<W>,
 {
     // Warmup
     {
@@ -332,50 +367,107 @@ where
     );
 }
 
-fn main() {
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn load_graph<P>(filename: P) -> io::Result<Vec<Vec<usize>>>
+where
+    P: AsRef<Path>,
+{
+    let mut graph = Vec::new();
+
+    let lines = read_lines(filename)?;
+    for line in lines {
+        let line = line?;
+        if line.starts_with("#") {
+            continue;
+        }
+
+        let mut it = line.split("\t");
+        let v: usize = it.next().unwrap().parse().unwrap();
+        let u: usize = it.next().unwrap().parse().unwrap();
+
+        if u >= graph.len() || v >= graph.len() {
+            graph.resize(v.max(u) + 1, vec![]);
+        }
+
+        graph[v].push(u);
+        graph[u].push(v);
+    }
+
+    Ok(graph)
+}
+
+fn main() -> io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
 
+    /*
     let mut n: usize = 1_000;
-    let mut p: f64 = 0.1;
+    let mut m: usize = 10;
     let mut num_rounds = 3;
 
     if args.len() >= 2 {
         n = args[1].parse().unwrap();
     }
     if args.len() >= 3 {
-        p = args[2].parse().unwrap();
+        m = args[2].parse().unwrap();
     }
     if args.len() >= 4 {
         num_rounds = args[3].parse().unwrap();
     }
 
     let n = n;
-    let p = p;
+    let m = m;
     let num_rounds = num_rounds;
 
-    println!("n = {0} p = {1} num_rounds = {2}", n, p, num_rounds);
+    println!("n = {0} m = {1} num_rounds = {2}", n, m, num_rounds);
+    */
 
-    println!("building graph...");
-    let graph = make_random_neighborhood_graph(n, p);
+    let num_rounds = 3;
+
+    println!("loading graph...");
+    let graph = load_graph(&args[1])?;
+
+    //println!("building graph...");
+    //let graph = make_random_graph(n, m);
+    //let graph = make_path_csr_graph(n);
 
     // Graph statistics
     {
+        let n = (&graph).num_vertices();
         let degree_sum: usize = (0..n).into_par_iter().map(|i| (&graph).degree(i)).sum();
+        let degree_min: usize = (0..n)
+            .into_par_iter()
+            .map(|i| (&graph).degree(i))
+            .min()
+            .unwrap();
 
         println!("edges: {}", degree_sum / 2);
+        println!("min degree: {}", degree_min);
         println!("max degree: {}", (&graph).max_degree());
         println!("average degree: {}", degree_sum as f64 / n as f64);
     }
 
+    println!();
     println!("random ordering:");
     test_coloring(&graph, make_random_order, num_rounds);
 
+    println!();
     println!("largest degree first ordering:");
     test_coloring(&graph, make_lf_order, num_rounds);
 
+    println!();
     println!("largest log degree first ordering:");
     test_coloring(&graph, make_llf_order, num_rounds);
 
+    println!();
     println!("smallest degree last ordering:");
     test_coloring(&graph, make_sl_order, num_rounds);
+
+    Ok(())
 }
